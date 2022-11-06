4f0ab8b86183cfa93d13587385fcf71161fad1b8
Rana Asad <benyameen75@gmail.com>
2022-11-06 01:48:29 -0700

    big

---
+++ cogs/test.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+
+class Test(commands.Cog):
+	def __init__(self, client):
+		self.bot = bot
+
+
+    @command.commands()
+    async def ping(ctx, self):
+        await ctx.send('pong!') 
+
+  
+
+
+def setup(bot):
+	bot.add_cog(Test(bot))
--- main.py
+++ main.py
@@ -12,4 +12,15 @@ async def on_ready():
 async def hallo(ctx):
     await ctx.send('hallo')
 
+extensions = [
+              "cogs.test"
+]
+if __name__ == "__main__":
+  for extension in extensions:
+    try:
+      bot.load_extension(extension)
+    except Exception as e:
+      print(f"error loading {extension}", file=sys.stderr)
+      traceback.print_exc()
+
 bot.run("Nzk3NDU1OTc1MTMyMTY4MjYz.GjUN1M.HS7HkLOXwSqNa_FFh9zuU6rdS0Ixmi0srzkho4")
