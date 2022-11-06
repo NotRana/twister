1cf8cfdaf6960f2428f13b5f49512d4f0302bc2c
Rana Asad <benyameen75@gmail.com>
2022-11-06 01:24:17 -0800

    lsfhsa

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def hallo(ctx):
     await ctx.send('hallo')
     
 
-@bot.slash_command(title="pong!", description="send asf pong!")
+@bot.command(title="pong!", description="send asf pong!")
 async def ping(ctx):
     await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
 # extensions = [
