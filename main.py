6320d7e1137e9605cf4eeb96f8589bae14197b82
Rana Asad <benyameen75@gmail.com>
2022-11-06 01:17:18 -0800

    fhsod

--- main.py
+++ main.py
@@ -11,16 +11,20 @@ async def on_ready():
 @bot.command()
 async def hallo(ctx):
     await ctx.send('hallo')
+    
 
-extensions = [
-              "cogs.test"
-]
-if __name__ == "__main__":
-  for extension in extensions:
-    try:
-      bot.load_extension(extension)
-    except Exception as e:
-      print(f"error loading {extension}", file=sys.stderr)
-      traceback.print_exc()
+@bot.command(title="pong!", description="send asf pong!")
+    async def ping(ctx):
+        await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
+# extensions = [
+#               "cogs.test"
+# ]
+# if __name__ == "__main__":
+#   for extension in extensions:
+#     try:
+#       bot.load_extension(extension)
+#     except Exception as e:
+#       print(f"error loading {extension}", file=sys.stderr)
+#       traceback.print_exc()
 
 bot.run("Nzk3NDU1OTc1MTMyMTY4MjYz.GjUN1M.HS7HkLOXwSqNa_FFh9zuU6rdS0Ixmi0srzkho4")
