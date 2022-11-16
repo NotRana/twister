6bb9b82c909df810fb95882e127adfd4e999f8e8
Rana Asad <benyameen75@gmail.com>
2022-11-16 07:09:00 -0800

    sad

--- cogs/ping.py
+++ cogs/ping.py
@@ -7,9 +7,9 @@ class ping(commands.Cog):
         self.bot = bot
 
 
-    @commands.slash_command(title="pong!", description="send asf pong!")
-    async def ping(ctx: discord.ApplicationContext, self):
-      await ctx.respond(f"Pong! {round(self.bot.latency * 1000)}ms")
+    @commands.slash_command()  # Not passing in guild_ids creates a global slash command.
+    async def ping(self, ctx: discord.ApplicationContext):
+        await ctx.respond(f"Pong! {round(self.bot.latency * 1000)}ms")
 
 
 def setup(bot):
