31c2a46ff9ac17fa139cec227fd67f5cae81c6fe
Rana Asad <benyameen75@gmail.com>
2022-11-16 06:43:52 -0800

    sad

--- test.py
+++ test.py
@@ -2,18 +2,15 @@ import discord
 from discord.ext import commands
 
 
-class Test(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
+class Example(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
 
-  @commands.slash_command()  # Not passing in guild_ids creates a global slash command.
-  async def hi(self, ctx: discord.ApplicationContext):
-    await ctx.respond("Hi, this is a global slash command from a cog!")
-
-  
+    @commands.slash_command()  # Not passing in guild_ids creates a global slash command.
+    async def hi(self, ctx: discord.ApplicationContext):
+        await ctx.respond("Hi, this is a global slash command from a cog!")
 
 
-adef setup(bot):
-  await bot.add_cog(Test(bot))
-  print('test cog is ready')
+def setup(bot):
+    bot.add_cog(Example(bot))
