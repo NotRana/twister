097317eef61dd5dc236d94e474931c44e5137db0
Rana Asad <benyameen75@gmail.com>
2022-12-18 06:27:01 -0800

    module add kr rha tha

--- main.py
+++ main.py
@@ -229,7 +229,7 @@ if __name__ == "__main__":
 
 @bot.command(name="meme")
 async def meme(ctx):
-  content = get("https://meme-api.herokuapp.com/gimme").text
+  content = get("https://rapidapi.com/collection/meme").text
   data = json.loads(content)
   meme = discord.Embed(title=f"{data['title']}", Color = discord.Color.random()).set_image(url=f"{data['url']}")
   await ctx.reply(embed=meme)
